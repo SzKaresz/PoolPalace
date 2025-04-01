72ec01c3cfca989497899c302a6c147a482362b4
SzKaresz <szautnerkaroly@gmail.com>
2025-04-01 09:06:22 +0200

    legordulőhöz js

--- files/php/termekOldal.php
+++ files/php/termekOldal.php
@@ -42,6 +42,7 @@ for ($i = 1; $i <= 5; $i++) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="icon" href="../img/logo_icon.ico" type="image/x-icon">
     <link rel="stylesheet" href="../css/termekOldal.css">
     <title><?php echo $termek['nev']; ?> - PoolPalace</title>
