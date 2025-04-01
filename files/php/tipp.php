78a3afb70ab45dbc91e73898c9c286c9a4b5c875
SzKaresz <szautnerkaroly@gmail.com>
2025-04-01 08:17:08 +0200

    tipp oldal

--- files/php/tipp.php
+++ files/php/tipp.php
@@ -26,7 +26,6 @@
         
         <p>Az otthoni medence karbantartása kulcsfontosságú ahhoz, hogy tiszta, biztonságos és élvezetes legyen a vízben való pihenés. A rendszeres karbantartás nemcsak a víz tisztaságát garantálja, hanem hosszú távon hozzájárul a medence élettartamának meghosszabbításához is. Az alábbiakban összegyűjtöttük a legfontosabb karbantartási feladatokat, amelyekkel otthon is könnyedén elvégezhetjük a medence ápolását.</p>
 
-        <!-- Grid 1: Víz kémiai egyensúlya -->
         <div class="row my-5">
             <h2 class="h3 my-4">1. A víz kémiai egyensúlyának fenntartása</h2>
             <div class="col-12 col-md-5 col-lg-3">
@@ -43,11 +42,10 @@
             </div>
         </div>
 
-        <!-- Grid 2: Víz tisztántartása -->
         <div class="row my-5">
             <h2 class="h3 my-4">2. A víz tisztán tartása – Szűrés és tisztítás</h2>
             <div class="col-12 col-md-5 col-lg-3">
-                <img src="https://via.placeholder.com/800x400?text=Sz%C5%B1r%C5%91rendszer+tiszt%C3%ADt%C3%A1sa" alt="Szűrőrendszer tisztítása" class="img-fluid mb-4 rounded image-left">
+                <img src="../img/tisztitas.png" alt="Szűrőrendszer tisztítása" class="img-fluid mb-4 rounded image-left">
             </div>
             <div class="col-12 col-md-7 col-lg-9">
                 <p>A víz tisztántartása érdekében elengedhetetlen a medence szűrőrendszerének megfelelő működése. A szűrőberendezés napi 6-8 órás működtetése szükséges ahhoz, hogy eltávolítsa a szennyeződéseket, mint a leveleket, törmelékeket és egyéb apró részecskéket. A szűrőt rendszeresen tisztítani kell, hogy hatékonyan működjön.</p>
@@ -59,33 +57,30 @@
             </div>
         </div>
 
-        <!-- Grid 3: Vízszint ellenőrzése -->
         <div class="row my-5">
             <h2 class="h3 my-4">3. A medence vízszintjének ellenőrzése</h2>
             <div class="col-12 col-md-5 col-lg-3">
-                <img src="https://via.placeholder.com/800x400?text=Medence+v%C3%ADzszint+ellen%C5%91rz%C3%A9se" alt="Medence vízszint ellenőrzése" class="img-fluid mb-4 rounded image-left">
+                <img src="../img/meres.png" alt="Medence vízszint ellenőrzése" class="img-fluid mb-4 rounded image-left">
             </div>
             <div class="col-12 col-md-7 col-lg-9">
                 <p>A medence vízszintje szintén kulcsfontosságú. A vízszint nemcsak a szűrési rendszer hatékonyságát befolyásolja, hanem a medence biztonságos használatát is. A vízszintet figyelemmel kell kísérni, különösen esős időszakok után, amikor a medence gyorsan túlfolyhat. A vízszint túl alacsony szintje viszont a szivattyú és a szűrő berendezések károsodásához vezethet.</p>
             </div>
         </div>
 
-        <!-- Grid 4: Algák és baktériumok ellen -->
         <div class="row my-5">
             <h2 class="h3 my-4">4. Algák és baktériumok ellen</h2>
             <div class="col-12 col-md-5 col-lg-3">
-                <img src="https://via.placeholder.com/800x400?text=Alg%C3%A1k+elt%C3%A1vol%C3%ADt%C3%A1sa" alt="Algák eltávolítása" class="img-fluid mb-4 rounded image-left">
+                <img src="../img/klorozas.png" alt="Algák eltávolítása" class="img-fluid mb-4 rounded image-left">
             </div>
             <div class="col-12 col-md-7 col-lg-9">
                 <p>A meleg víz és az alacsony klórszint ideális környezetet biztosít az algák és baktériumok szaporodásához. Az algák megelőzésére és eltávolítására speciális vegyszereket használhatunk. Az algairtó szerek és a rendszeres klórozás segítenek megakadályozni az algák növekedését. Ha már jelen vannak, akkor a medencetisztító kefék és robotok használata is hasznos lehet.</p>
             </div>
         </div>
 
-        <!-- Grid 5: Téli karbantartás -->
         <div class="row my-5">
             <h2 class="h3 my-4">5. Medence téli karbantartása</h2>
             <div class="col-12 col-md-5 col-lg-3">
-                <img src="https://via.placeholder.com/800x400?text=Medence+t%C3%A9li+karbantart%C3%A1sa" alt="Medence téli karbantartása" class="img-fluid mb-4 rounded image-left">
+                <img src="../img/teli.png" alt="Medence téli karbantartása" class="img-fluid mb-4 rounded image-left">
             </div>
             <div class="col-12 col-md-7 col-lg-9">
                 <p>Ha nem használjuk a medencét a téli hónapokban, fontos, hogy megfelelően felkészítsük a hideg időszakra. A medence téli karbantartása magában foglalja a víz leeresztését, a szűrőrendszer kiszerelését, a medence alapos tisztítását és a medence lefedését védőfóliával. Az extrém hidegben megfagyhat a víz, ami károsíthatja a medencét, ezért érdemes figyelni a megfelelő lefolyást és szigetelést.</p>
